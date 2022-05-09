e3525aa3aaf6b21c083209acd2fe132cbbb10c02
lsjpjs1 <lsjpjs1@naver.com>
2022-05-09 19:26:06 +0900

    fix: 크롤링 꺼지는 버그 수정

--- src/main/java/com/example/restapimvc/scraper/common/AbstractJobScraper.java
+++ src/main/java/com/example/restapimvc/scraper/common/AbstractJobScraper.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -24,7 +25,15 @@ public abstract class AbstractJobScraper implements JobScraper{
     @Override
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-        this.driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-setuid-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        this.driver = new ChromeDriver(options);
+
         goInitPage();
     }
     public void wait(By by,long maxWaitingTime) {
--- src/test/java/com/example/restapimvc/scraper/site/prospects/ProspectsScraperTest.java
+++ src/test/java/com/example/restapimvc/scraper/site/prospects/ProspectsScraperTest.java
@@ -21,6 +21,62 @@ class ProspectsScraperTest {
     private static final String today = new SimpleDateFormat("yyyyMMdd").format(Calendar.getInstance().getTime());
 
     @Test
+    void runJobs() throws InterruptedException {
+        Thread prospectsThread = new Thread(() -> {
+            ProspectsScraper prospectsScraper = new ProspectsScraper();
+            try {
+                prospectsScraper.startScraping();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                insertJobService.insertJobs(prospectsScraper.getFindTargets());
+                CsvUtil.toCsv(prospectsScraper.getFindTargets(), "/Users/sunghonpark/RESTApiMVC/src/main/java/com/example/scraper/result/res_prospects_" + today + ".csv", false);
+            }
+        });
+
+        Thread brightNetworkThread = new Thread(() -> {
+            BrightNetworkScraper brightNetworkScraper = new BrightNetworkScraper();
+            try {
+                brightNetworkScraper.startScraping();
+            } catch (Exception e){
+                e.printStackTrace();
+            } finally {
+                insertJobService.insertJobs(brightNetworkScraper.getFindTargets());
+                CsvUtil.toCsv(brightNetworkScraper.getFindTargets(),"/Users/sunghonpark/RESTApiMVC/src/main/java/com/example/scraper/result/res_bright_network_"+today+".csv",false);
+            }
+        });
+
+        Thread indeedThread = new Thread(() -> {
+            IndeedScraper indeedScraper = new IndeedScraper(0, "graduate scheme","London, Greater London");
+            try {
+                indeedScraper.startScraping();
+            } catch (Exception e){
+                e.printStackTrace();
+            } finally {
+                insertJobService.insertJobs(indeedScraper.getFindTargets());
+                CsvUtil.toCsv(indeedScraper.getFindTargets(),"/Users/sunghonpark/RESTApiMVC/src/main/java/com/example/scraper/result/res_indeed_"+today+".csv",false);
+            }
+        });
+
+        Thread targetJobsThread = new Thread(() -> {
+            TargetJobsScraper targetJobsScraper = new TargetJobsScraper();
+            try {
+                targetJobsScraper.startScraping();
+            } catch (Exception e){
+                e.printStackTrace();
+            } finally {
+                insertJobService.insertJobs(targetJobsScraper.getFindTargets());
+                CsvUtil.toCsv(targetJobsScraper.getFindTargets(),"/Users/sunghonpark/RESTApiMVC/src/main/java/com/example/scraper/result/res_targetjobs_"+today+".csv",false);
+            }
+        });
+
+        prospectsThread.start();
+        brightNetworkThread.start();
+        indeedThread.start();
+        targetJobsThread.start();
+        Thread.sleep(10000000l);
+    }
+    @Test
     void prospects() {
         ProspectsScraper prospectsScraper = new ProspectsScraper();
         try {
