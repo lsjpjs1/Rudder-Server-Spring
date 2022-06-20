c87ecfe82cceee815a9bf37b6191f027413b500e
lsjpjs1 <lsjpjs1@naver.com>
2022-06-20 23:29:03 +0900

    fix: 메시지큐 클라우드 서버 주소로 수정

--- src/main/java/com/example/restapimvc/config/WebSocketConfig.java
+++ src/main/java/com/example/restapimvc/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.example.restapimvc.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
@@ -10,15 +11,26 @@ import org.springframework.web.socket.config.annotation.*;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/pub")
                 .enableStompBrokerRelay("/topic")
-                .setRelayHost("localhost")
-                .setVirtualHost("/")
+                .setRelayHost(host)
                 .setRelayPort(61613)
-                .setClientLogin("guest")
-                .setClientPasscode("guest");
+                .setClientLogin(username)
+                .setClientPasscode(password)
+                .setSystemLogin(username)
+                .setSystemPasscode(password);
+
     }
 
     @Override
