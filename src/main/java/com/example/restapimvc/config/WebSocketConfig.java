eaff8e7d0ec9ddd1aad4060db1ebcc1d1e9aa030
lsjpjs1 <lsjpjs1@naver.com>
2022-07-19 15:32:48 +0900

    fix: 소켓 큐 topic에서 queue로 수정

--- src/main/java/com/example/restapimvc/config/WebSocketConfig.java
+++ src/main/java/com/example/restapimvc/config/WebSocketConfig.java
@@ -23,7 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/pub")
-                .enableStompBrokerRelay("/topic")
+                .enableStompBrokerRelay("/queue")
                 .setRelayHost(host)
                 .setRelayPort(61613)
                 .setClientLogin(username)
