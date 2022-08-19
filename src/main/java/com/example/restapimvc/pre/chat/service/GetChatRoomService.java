705494981a32ca6b900c0a174458fe03274396af
lsjpjs1 <lsjpjs1@naver.com>
2022-08-19 16:38:17 +0900

    fix: 채팅방 사진 하드코딩 된 것 수정

--- src/main/java/com/example/restapimvc/pre/chat/service/GetChatRoomService.java
+++ src/main/java/com/example/restapimvc/pre/chat/service/GetChatRoomService.java
@@ -1,13 +1,18 @@
 package com.example.restapimvc.pre.chat.service;
 
 import com.example.restapimvc.domain.UserInfo;
+import com.example.restapimvc.domain.UserProfile;
 import com.example.restapimvc.exception.CustomException;
 import com.example.restapimvc.exception.ErrorCode;
 import com.example.restapimvc.pre.chat.ChatDto;
 import com.example.restapimvc.pre.chat.repository.ChatRoomMemberRepository;
 import com.example.restapimvc.pre.party.command.domain.Party;
 import com.example.restapimvc.pre.party.command.domain.PartyRepository;
+import com.example.restapimvc.repository.UserInfoRepository;
+import com.example.restapimvc.repository.UserPartyProfileImageRepository;
+import com.example.restapimvc.repository.UserProfileImageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,8 +28,11 @@ public class GetChatRoomService {
 
     private final ChatRoomMemberRepository chatRoomMemberRepository;
     private final PartyRepository partyRepository;
+    private final UserInfoRepository userInfoRepository;
+    private final UserPartyProfileImageRepository userPartyProfileImageRepository;
 
-
+    @Value("${cloud-front.url.post-image}")
+    private String CLOUD_FRONT_POST_IMAGE_URL;
     @Transactional
     public ChatDto.GetChatRoomsResponse getHostPartyOneToOneChatRooms(UserInfo userInfo, Long partyId) {
         List<Tuple> tuples = chatRoomMemberRepository.findHostPartyOneToOneChatRooms(partyId,userInfo.getUserInfoId());
@@ -34,7 +42,7 @@ public class GetChatRoomService {
                         .recentMessage(tuple.get(2, String.class))
                         .recentMessageTime(tuple.get(3, Timestamp.class))
                         .notReadMessageCount(tuple.get(4, BigInteger.class).intValue())
-                        .chatRoomImageUrl("http://d17a6yjghl1rix.cloudfront.net/1657524178034514172")
+                        .chatRoomImageUrl(CLOUD_FRONT_POST_IMAGE_URL+userPartyProfileImageRepository.findById(userInfoRepository.findById(tuple.get(7,Integer.class).longValue()).get().getUserPartyProfile().getUserPartyProfileImageId()).get().getPartyProfileImageName())
                         .chatRoomTitle(tuple.get(5,String.class)+" + "+tuple.get(6,Integer.class))
                         .otherUserInfoId(tuple.get(7,Integer.class).longValue())
                         .build()
@@ -52,7 +60,7 @@ public class GetChatRoomService {
                         .recentMessage(tuple.get(2, String.class))
                         .recentMessageTime(tuple.get(3, Timestamp.class))
                         .notReadMessageCount(tuple.get(4, BigInteger.class).intValue())
-                        .chatRoomImageUrl("http://d17a6yjghl1rix.cloudfront.net/1657524178034514172")
+                        .chatRoomImageUrl(CLOUD_FRONT_POST_IMAGE_URL+userPartyProfileImageRepository.findById(userInfoRepository.findById(tuple.get(6,Integer.class).longValue()).get().getUserPartyProfile().getUserPartyProfileImageId()).get().getPartyProfileImageName())
                         .chatRoomTitle(tuple.get(5,String.class))
                         .otherUserInfoId(tuple.get(6,Integer.class).longValue())
                         .build()
@@ -72,7 +80,7 @@ public class GetChatRoomService {
                 .recentMessage(tuple.get(2, String.class))
                 .recentMessageTime(tuple.get(3, Timestamp.class))
                 .notReadMessageCount(tuple.get(4, BigInteger.class).intValue())
-                .chatRoomImageUrl("http://d17a6yjghl1rix.cloudfront.net/1660714564504420051")
+                .chatRoomImageUrl(CLOUD_FRONT_POST_IMAGE_URL+partyRepository.findById(getPartyGroupChatRoomRequest.getPartyId()).get().getPartyThumbnailName())
                 .chatRoomTitle(party.getPartyTitle()+" "+ tuple.get(5, BigInteger.class))
                 .build();
     }
--- src/test/java/com/example/restapimvc/pre/chat/controller/ChatRoomControllerTest.java
+++ src/test/java/com/example/restapimvc/pre/chat/controller/ChatRoomControllerTest.java
@@ -25,7 +25,7 @@ class ChatRoomControllerTest {
     @Test
     void getPartyGroupChatRoom() throws Exception {
         mockMvc.perform(
-                        get("/chat-rooms/party-group/3")
+                        get("/chat-rooms/party-group/17")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .accept(MediaType.APPLICATION_JSON)
                                 .header("Authorization","Bearer eyJhbGciOiJIUzUxMiJ9.eyJwcm9tb3Rpb25NYWlsQWdyZWVtZW50IjpmYWxzZSwiYXV0aCI6IlJPTEVfVVNFUiIsInNjaG9vbCI6eyJzY2hvb2xJZCI6NCwic2Nob29sTmFtZSI6Im5hdmVyIiwicmVnZXgiOiJtaHBhcmswMjIwQG5hdmVyLmNvbSJ9LCJ1c2VyTmlja25hbWUiOiJhZG1pbm0iLCJ1c2VyRW1haWwiOiJtaHBhcmswMjIwQG5hdmVyLmNvbSIsInVzZXJUeXBlIjowLCJ1c2VySWQiOiJtaHBhcmswMjIwQG5hdmVyLmNvbSIsInVzZXJJbmZvSWQiOjM0Nywibm90aWZpY2F0aW9uVG9rZW4iOiJyaWdodENhc2UifQ.c5ByggYQazfIK1tf0TvFf7Zg3VH4nWoQtX3o_9DV9rSa9uovGC9G4Bd9O92CagFsl10DLjLeNiV8dqbFxzMSfg")
@@ -37,7 +37,7 @@ class ChatRoomControllerTest {
     @Test
     void getHostPartyOneToOneChatRooms() throws Exception {
         mockMvc.perform(
-                        get("/chat-rooms/party-one-to-one/2")
+                        get("/chat-rooms/party-one-to-one/17")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .accept(MediaType.APPLICATION_JSON)
                                 .header("Authorization","Bearer eyJhbGciOiJIUzUxMiJ9.eyJwcm9tb3Rpb25NYWlsQWdyZWVtZW50IjpmYWxzZSwiYXV0aCI6IlJPTEVfVVNFUiIsInNjaG9vbCI6eyJzY2hvb2xJZCI6NCwic2Nob29sTmFtZSI6Im5hdmVyIiwicmVnZXgiOiJtaHBhcmswMjIwQG5hdmVyLmNvbSJ9LCJ1c2VyTmlja25hbWUiOiJhZG1pbm0iLCJ1c2VyRW1haWwiOiJtaHBhcmswMjIwQG5hdmVyLmNvbSIsInVzZXJUeXBlIjowLCJ1c2VySWQiOiJtaHBhcmswMjIwQG5hdmVyLmNvbSIsInVzZXJJbmZvSWQiOjM0Nywibm90aWZpY2F0aW9uVG9rZW4iOiJyaWdodENhc2UifQ.c5ByggYQazfIK1tf0TvFf7Zg3VH4nWoQtX3o_9DV9rSa9uovGC9G4Bd9O92CagFsl10DLjLeNiV8dqbFxzMSfg")
